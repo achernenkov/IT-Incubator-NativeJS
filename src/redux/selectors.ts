29039f6ccf5dcf68387fb7a7012aebf46062da56
achernenkov <jardagorng@gmail.com>
2020-12-16 22:33:29 +0300

    Refactoring

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -3,10 +3,4 @@ import {IGlobalState} from './state'
 interface IRootStat extends IGlobalState {
 }
 
-export const selectCurrencies = (state: IRootStat) => state.currency.currencies
-export const selectUrrentCurrency = (state: IRootStat) => state.currency.currentCurrency
-export const selectIsBuying = (state: IRootStat) => state.currency.isBuying
-export const selectAmountOfBYN = (state: IRootStat) => state.currency.amountOfBYN
-export const selectAmountOfCurrency = (state: IRootStat) => state.currency.amountOfCurrency
-
 export const selectState = (state: IRootStat) => state.currency

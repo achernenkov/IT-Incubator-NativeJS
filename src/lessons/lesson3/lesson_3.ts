7e076f1137738932a79c03579bb108b3d920515f
achernenkov <jardagorng@gmail.com>
2021-01-09 21:10:35 +0300

    Finish test jsonplaceholder API.

--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 console.log('lesson 3');
 
 // Event loop
@@ -14,6 +16,55 @@ console.log('lesson 3');
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
 
+
+const jsonplaceholderAPI = {
+    baseURL: 'https://jsonplaceholder.typicode.com/'
+}
+
+const l3API = axios.create(jsonplaceholderAPI)
+
+// get
+
+l3API.get('posts/1').then(respons => {
+    console.log('One Post', respons.data)
+})
+
+l3API.get('posts').then(respons => {
+    console.log('Many Post', respons.data)
+})
+
+
+// post
+
+l3API.post(
+    'posts',
+    {
+        title: 'foo',
+        body: 'bar',
+        userId: 1
+    }
+).then(respons => {
+    console.log('post request, response', respons)
+})
+
+
+// put
+
+l3API.put('posts/1', {
+    id: 1,
+    title: 'foo',
+    body: 'bar',
+    userId: 1
+}).then(respons => {
+    console.log('put request', respons)
+})
+
+
+l3API.patch('posts/1', {
+    title: 'NoFOOO'
+}).then(respons => {
+    console.log('Patch request', respons)
+})
 
 // just a plug
 export default ()=>{};
